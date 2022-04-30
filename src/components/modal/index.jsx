2da354d274537add1e06faf8e815cd5979ce2b9a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-04-30 21:14:11 +0700

    feat(modal): add children

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,8 +1,23 @@
-const ModalBody = () => {};
+import ReactDOM from "react-dom";
 
-const ModalFooter = () => {};
+const ModalBody = ({ children = null }) => {
+  return <main>{children}</main>;
+};
 
-const Modal = () => {};
+const ModalFooter = ({ children = null }) => {
+  return <footer>{children}</footer>;
+};
+
+const Modal = ({ children = null, open = false }) => {
+  if (!open) return null;
+
+  return ReactDOM.createPortal(
+    <div>{children}</div>,
+    document.getElementById("root")
+  );
+};
+
+Modal.Header = ModalHeader;
 
 Modal.Body = ModalBody;
 
