a3649065293826458eebbaf7d4c38e3226cb45b0
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-04-30 21:36:17 +0700

    feat(App): add toggle modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import { Button } from "@mui/material";
+import { useState } from "react";
+import Modal from "./components/modal";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen((isOpen) => !isOpen);
+
   return (
     <div>
-      <Button variant="contained">Open Modal</Button>
+      <Modal open={isOpen} title="Simple Modal" onClose={toggle} />
+      <Button variant="contained" onClick={toggle}>
+        Open Modal
+      </Button>
     </div>
   );
 }
