fde176e98ebb248544f6d8d95441bb392b9b1232
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2022-04-30 21:36:01 +0700

    feat(modal): styling modal

--- src/components/modal/index.jsx
+++ src/components/modal/index.jsx
@@ -1,3 +1,4 @@
+import { Box, Typography } from "@mui/material";
 import ReactDOM from "react-dom";
 
 const ModalBody = ({ children = null }) => {
@@ -8,11 +9,47 @@ const ModalFooter = ({ children = null }) => {
   return <footer>{children}</footer>;
 };
 
-const Modal = ({ children = null, open = false }) => {
+const Modal = ({
+  children = null,
+  open = false,
+  title = "",
+  onClose = () => {},
+}) => {
   if (!open) return null;
 
   return ReactDOM.createPortal(
-    <div>{children}</div>,
+    <>
+      <Box
+        position="fixed"
+        zIndex={20}
+        top="50%"
+        left="50%"
+        sx={{ transform: "translate(-50%, -50%)" }}
+        boxShadow="rgba(149, 157, 165, 0.2) 0px 8px 24px;"
+        bgcolor="white"
+        p="1rem"
+        borderRadius=".5rem"
+        width="500px"
+      >
+        <Box display="flex" alignItems="center" justifyContent="space-between">
+          <Typography variant="h1" fontSize="1.5rem" fontWeight="bold">
+            {title}
+          </Typography>
+          <Typography variant="caption" onClick={onClose}>
+            close
+          </Typography>
+        </Box>
+      </Box>
+      <Box
+        position="fixed"
+        zIndex={10}
+        bgcolor="rgba(0, 0, 0, 0.5)"
+        width="100%"
+        height="100%"
+        top={0}
+        left={0}
+      />
+    </>,
     document.getElementById("root")
   );
 };
